fab1b4efd407e082d18709c79d93ec4aaaf8688d
Ali <alimammadsoy@gmail.com>
2026-01-20 14:59:26 +0400

    first migration added, program.cs configured

---
+++ src/Infrastructure/Jobby.Persistence/Migrations/20260120105529_FirstMigrationUserEntityAdded.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Jobby.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigrationUserEntityAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    full_name = table.Column<string>(type: "text", nullable: true),
+                    email = table.Column<string>(type: "text", nullable: true),
+                    password_hash = table.Column<string>(type: "text", nullable: true),
+                    refresh_token = table.Column<string>(type: "text", nullable: true),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    created_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
--- src/Presentation/Jobby.WebApi/Program.cs
+++ src/Presentation/Jobby.WebApi/Program.cs
@@ -1,24 +1,244 @@
+using Jobby.Application;
+using Jobby.Persistence;
+using Jobby.Persistence.Context;
+using Jobby.WebApi.Configuration;
+using Jobby.WebApi.Middlewares;
+using Jobby.WebApi.SeedDatas;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+            //.AllowCredentials();
+        });
+});
 
 
-builder.Services.AddControllers();
+builder.Services.AddPersistenceServices(builder.Configuration);
+builder.Services.AddApplicationServices();
+
+
+builder.Services.AddControllers(opt =>
+{
+    opt.Conventions.Add(new RouteTokenTransformerConvention(new KebabCaseParameterTransformer()));
+})
+.AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new AzerbaijanDateTimeConverter());
+    options.JsonSerializerOptions.Converters.Add(new NullableAzerbaijanDateTimeConverter());
+});
+
+
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddHttpClient();
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ApplicationSeedDbContext>();
+
+builder.Services.AddHttpLogging(logging =>
+{
+    logging.LoggingFields = HttpLoggingFields.All;
+    logging.RequestHeaders.Add("sec-chu-ua");
+    logging.MediaTypeOptions.AddText("application/javascript");
+    logging.RequestBodyLogLimit = 4096;
+    logging.ResponseBodyLogLimit = 4096;
+});
+
+builder.Logging.ClearProviders();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    var securityScheme = new OpenApiSecurityScheme
+    {
+        Name = "JWT Authentication",
+        Description = "Enter your JWT token in this field",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+
+    c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { securityScheme, new string[] { } }
+    });
+});
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddHealthChecks();
+
+builder.Services.Configure<KestrelServerOptions>(options =>
+{
+    options.AddServerHeader = false;
+    options.Limits.MaxRequestBodySize = int.MaxValue;
+});
+
+builder.Services.Configure<FormOptions>(x =>
+{
+    x.ValueLengthLimit = int.MaxValue;
+    x.MultipartBodyLengthLimit = int.MaxValue;
+});
+
+
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+
+}).AddJwtBearer(opt =>
+{
+    opt.RequireHttpsMetadata = false;
+    opt.SaveToken = true;
+
+    opt.TokenValidationParameters = new()
+    {
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration.GetSection("JWT:Audience").Value,
+
+        ValidateIssuer = true,
+        ValidIssuer = builder.Configuration.GetSection("JWT:Issuer").Value,
+
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("JWT:SecretKey").Value)),
+
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero
+    };
+    opt.Events = new JwtBearerEvents()
+    {
+        OnMessageReceived = context =>
+        {
+            context.Token = context.Request.Cookies["X-Access-Token"];
+            return Task.CompletedTask;
+        },
+
+        OnTokenValidated = context =>
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = context.SecurityToken as JwtSecurityToken;
+
+            if (token == null || token.ValidTo < DateTime.UtcNow) // Token expired?
+            {
+                context.Fail("Token has expired.");
+            }
+
+
+            return Task.CompletedTask;
+        },
+
+        OnAuthenticationFailed = context =>
+        {
+            return Task.CompletedTask;
+        },
+        OnForbidden = context =>
+        {
+            var token = context.Request.Cookies["X-Access-Token"];
+            return Task.CompletedTask;
+        }
+    };
+}).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
+
+builder.Services.AddAuthorization();
+
+var env = builder.Environment;
+
+builder.Configuration
+    .SetBasePath(env.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional: false)
+    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+
 
 var app = builder.Build();
 
+app.UseMiddlewares();
+
+/*app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Jobby.API V1");
+    c.DocumentTitle = "Jobby API";
+    c.DocExpansion(DocExpansion.List);
+});
+app.UseDeveloperExceptionPage();*/
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Jobby.API V1");
+        c.DocumentTitle = "Jobby API";
+        c.DocExpansion(DocExpansion.List);
+    });
+    //app.UseDeveloperExceptionPage();
 }
+
+app.Use(async (context, next) =>
+{
+    context.Response.Headers["Cache-Control"] = "no-store, no-cache, must-revalidate";
+    context.Response.Headers["Pragma"] = "no-cache";
+    context.Response.Headers["Expires"] = "0";
+    await next();
+});
+
+/*app.UseStaticFiles(new StaticFileOptions
+{
+    RequestPath = "/files"
+});
+*/
+app.UseSerilogRequestLogging();
+app.UseHttpLogging();
+app.UseCors("AllowAllOrigins");
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    context.Database.Migrate();
+    var logger = services.GetRequiredService<ILogger<ApplicationSeedDbContext>>();
+    var seed = services.GetRequiredService<ApplicationSeedDbContext>();
+    await seed.SeedAsync(context, logger);
+}
+
+app.MapFallbackToFile("index.html");
 app.Run();
