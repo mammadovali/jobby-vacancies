d25bdae6940dde949403937b715a86902e089226
Ali <alimammadsoy@gmail.com>
2026-01-20 22:40:14 +0400

    category entity class created

---
+++ src/Core/Jobby.Domain/Entities/CategoryAggregate/Category.cs
@@ -0,0 +1,17 @@
+﻿using Jobby.Domain.Entities.Common;
+using Jobby.Domain.Entities.Identity;
+
+namespace Jobby.Domain.Entities.CategoryAggregate
+{
+    public class Category : Editable<User>
+    {
+        public string Name { get; private set; }
+        public bool IsActive { get; private set; }
+
+        public void SetDetails(string name, bool isActive)
+        {
+            Name = name;
+            IsActive = isActive;
+        }
+    }
+}
--- src/Core/Jobby.Domain/Entities/Common/Editable.cs
+++ src/Core/Jobby.Domain/Entities/Common/Editable.cs
@@ -4,14 +4,14 @@ namespace Jobby.Domain.Entities.Common
 {
     public class Editable<TUser> : Auditable<TUser> where TUser : User
     {
-        public int? UpdateById { get; protected set; }
-        public DateTime? LastUpdateDateTime { get; protected set; }
+        public int? UpdatedById { get; protected set; }
+        public DateTime? UpdatedDate { get; protected set; }
 
         public void SetEditFields(int? updatedById)
         {
 
-            UpdateById = updatedById;
-            LastUpdateDateTime = DateTime.UtcNow;
+            UpdatedById = updatedById;
+            UpdatedDate = DateTime.UtcNow;
         }
     }
 }
