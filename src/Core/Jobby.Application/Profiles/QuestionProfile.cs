7fddfb076b1e68641ebbcdde2c00f3a14b2f8fc0
alimammadsoy <alimammadsoy@gmail.com>
2026-01-21 22:43:24 +0400

    QuestionProfile addded

---
+++ src/Core/Jobby.Application/Profiles/QuestionProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Jobby.Application.Features.Queries.Question.DTOs;
+
+namespace Jobby.Application.Profiles
+{
+    public class QuestionProfile : Profile
+    {
+        public QuestionProfile()
+        {
+            CreateMap<Domain.Entities.QuestionAggregate.Question, QuestionDto>();
+            CreateMap<Domain.Entities.QuestionAggregate.QuestionOption, QuestionOptionResponseDto>();
+        }
+    }
+}
